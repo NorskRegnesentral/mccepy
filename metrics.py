62eaa7f0d8fe8c64fdbd0ee581e9f13a87c50815
Annabelle Redelmeier <aredelmeier@gmail.com>
2022-05-25 13:58:57 +0200

    changed metrics script to work for the carla methods

--- metrics.py
+++ metrics.py
@@ -1,230 +1,90 @@
 import numpy as np
-import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 
-def get_delta(instance: np.ndarray, cf: np.ndarray):
-    """
-    Compute difference between original instance and counterfactual
-    Parameters
-    ----------
-    instance: np.ndarray
-        Normalized and encoded array with factual data.
-        Shape: NxM
-    cf: : np.ndarray
-        Normalized and encoded array with counterfactual data.
-        Shape: NxM
-    Returns
-    -------
-    np.ndarray
-    """
-    return cf - instance
 
-def d1_distance(delta: np.ndarray): # L0 norm
-    """
-    Computes Sparsity
-    Parameters
-    ----------
-    delta: np.ndarray
-        Difference between factual and counterfactual
-    Returns
-    -------
-    List[float]
-    """
-    # compute elements which are greater than 0
-    return np.sum(delta != 0, axis=1, dtype=float).tolist()
-
-
-def d2_distance(delta: np.ndarray): # L1 norm
-    """
-    Computes D2 distance
-    Parameters
-    ----------
-    delta: np.ndarray
-        Difference between factual and counterfactual
-    Returns
-    -------
-    List[float]
-    """
-
-    return np.sum(np.abs(delta), axis=1, dtype=float).tolist()
-
-def d3_distance(delta: np.ndarray): # L2 norm
-    """
-    Computes Euclidean distance
-    Parameters
-    ----------
-    delta: np.ndarray
-        Difference between factual and counterfactual
-    Returns
-    -------
-    List[float]
-    """
-    return np.sum(np.square(np.abs(delta)), axis=1, dtype=np.float).tolist()
-
-
-def yNN(
-    data,
-    counterfactuals,
-    response,
-    y):
-    """
-    Parameters
-    ----------
-    data: pd.DataFrame, original training data
-    counterfactuals: pd.DataFrame, possible counterfactuals
-    response: string, name of response
-    y: Number of neighbours
-    Returns
-    -------
-    List[float]
-    """
+def distance(counterfactuals_without_nans, factual_without_nans, ml_model):
     
-    results = []
-    N = counterfactuals.shape[0]
-
-    nbrs = NearestNeighbors(n_neighbors=y).fit(data.values) # data includes response
-
-    for i, row in counterfactuals.iterrows():
-        knn = nbrs.kneighbors(row.values.reshape((1, -1)), y, return_distance=False)[0]
-        cf_label = row[response]
         
-        number_of_diff_labels = 0
-        for idx in knn:
-            neighbour = data.iloc[idx]
-            
-            neighbour_label = neighbour[response]
+    arr_f = ml_model.get_ordered_features(factual_without_nans).to_numpy()
+    arr_cf = ml_model.get_ordered_features(
+        counterfactuals_without_nans
+    ).to_numpy()
 
-            number_of_diff_labels += np.abs(cf_label - neighbour_label)
-        results.append(1 - (1 / (N * y)) * number_of_diff_labels)
+    delta = arr_f - arr_cf 
 
-    # print(1 - (1 / (N * y)) * number_of_diff_labels)
-    return results
+    d1 = np.sum(np.invert(np.isclose(delta, np.zeros_like(delta))), axis=1, dtype=np.float).tolist()
+    d1_old = np.sum(delta.round(2) != 0, axis=1, dtype=np.float).tolist()
+
+    d2 = np.sum(np.abs(delta), axis=1, dtype=np.float).tolist()
+    d3 = np.sum(np.square(np.abs(delta)), axis=1, dtype=np.float).tolist()
+
+    return({'L0': d1, 'L1': d2, 'L2': d3})
+
+
 
 def feasibility(
-    data,
-    counterfactuals,
-    response,
-    y):
-    """
-    Parameters
-    ----------
-    data: pd.DataFrame, original training data
-    counterfactuals: pd.DataFrame, possible counterfactuals
-    response: string, name of response
-    y: Number of neighbours
-    Returns
-    -------
-    List[float]
-    """
-    cols = data.columns
-    cols.drop(response)
+    counterfactuals_without_nans,
+    factual_without_nans,
+    dataset
+    ):
+    
+
+    cols = dataset.df.columns
+    cols.drop(dataset.target)
+
+    nbrs = NearestNeighbors(n_neighbors=5).fit(factual_without_nans[cols].values)
 
     results = []
-    nbrs = NearestNeighbors(n_neighbors=y).fit(data[cols].values)
-
-    for i, row in counterfactuals[cols].iterrows():
-        knn = nbrs.kneighbors(row.values.reshape((1, -1)), y, return_distance=True)[0]
+    for i, row in counterfactuals_without_nans[cols].iterrows():
+        knn = nbrs.kneighbors(row.values.reshape((1, -1)), 5, return_distance=True)[0]
         
         results.append(np.mean(knn))
+
     return results
-
-
-
-def redundancy(
-    counterfactuals,
-    factuals,
-    model,
-    response
-):
-    """
-    Parameters
-    ----------
-    counterfactuals: pd.DataFrame, possible counterfactuals
-    factuals: pd.DataFrame, original test observations
-    model: fit model object
-    response: string, name of response
-    Returns
-    -------
-    List[float]
-    """
-
-    df_fact = factuals.reset_index(drop=True)
-    df_cfs = counterfactuals.reset_index(drop=True)
-
-    labels = df_cfs[response]
-    df_cfs = df_cfs.drop(response, axis=1)
-    df_fact = df_fact.drop(response, axis=1)
-    
-    df_cfs["redundancy"] = df_cfs.apply(
-        lambda x: compute_redundancy(
-            df_fact.iloc[x.name].values, x.values, model, labels.iloc[x.name]
-        ),
-        axis=1,
-    )
-    return df_cfs["redundancy"].values.reshape((-1, 1))
-
-
-def compute_redundancy(
-    fact, 
-    cf, 
-    mlmodel, 
-    label_value):
-    red = 0
-    for col_idx in range(cf.shape[0]):  # input array has one-dimensional shape
-        if fact[col_idx] != cf[col_idx]:
-            temp_cf = np.copy(cf)
-
-            temp_cf[col_idx] = fact[col_idx]
-
-            temp_pred = mlmodel.predict(temp_cf.reshape((1, -1)))
-
-            if temp_pred == label_value:
-                red += 1
-
-    return red
 
 
 def constraint_violation(
-    counterfactuals, 
-    factuals,
-    cont_feat,
-    fixed_features,
-    scaler=None,
-):
-    """
-    Parameters
-    ----------
-    counterfactuals: pd.DataFrame, possible counterfactuals
-    factuals: pd.DataFrame, original test observations
-    model: fit model object
-    cont_feat: list, continuous features
-    fixed_feat: list, fixed features
-    scaler: if continuous features have been scaled, pass scaler here
-    Returns
-    -------
-    List[float]
-    """
-    immutables = fixed_features
-
-    df_decoded_cfs = counterfactuals.copy()
-    df_decoded_fact = factuals.copy()
-
-    if scaler: 
-        # Decode counterfactuals to compare immutables with not encoded factuals
-        df_decoded_cfs[cont_feat] = scaler.inverse_transform(df_decoded_cfs[cont_feat])
-        df_decoded_cfs[cont_feat] = df_decoded_cfs[cont_feat].astype("int64")  # avoid precision error
-
-        df_decoded_fact[cont_feat] = scaler.inverse_transform(df_decoded_fact[cont_feat])
-        df_decoded_fact[cont_feat] = df_decoded_fact[cont_feat].astype("int64")  # avoid precision error
+    counterfactuals_without_nans, 
+    factual_without_nans,
+    dataset,
+    ):
     
+    def intersection(lst1, lst2):
+        return list(set(lst1) & set(lst2))
 
-    df_decoded_cfs = df_decoded_cfs[immutables]
-    df_factuals = df_decoded_fact[immutables]
 
-    logical = df_factuals != df_decoded_cfs
-    logical = np.sum(logical.values, axis=1).reshape((-1, 1))
-    
-    return logical
+    df_decoded_cfs = dataset.inverse_transform(counterfactuals_without_nans.copy())
+
+    df_factuals = dataset.inverse_transform(factual_without_nans.copy())
+
+    cfs_continuous_immutable = df_decoded_cfs[
+        intersection(dataset.continuous, dataset.immutables)
+    ]
+    factual_continuous_immutable = df_factuals[
+        intersection(dataset.continuous, dataset.immutables)
+    ]
+
+    continuous_violations = np.invert(
+        np.isclose(cfs_continuous_immutable, factual_continuous_immutable)
+    )
+    continuous_violations = np.sum(continuous_violations, axis=1).reshape(
+        (-1, 1)
+    )  # sum over features
+
+    # check categorical by boolean comparison
+    cfs_categorical_immutable = df_decoded_cfs[
+        intersection(dataset.categorical, dataset.immutables)
+    ]
+    factual_categorical_immutable = df_factuals[
+        intersection(dataset.categorical, dataset.immutables)
+    ]
+
+    categorical_violations = cfs_categorical_immutable != factual_categorical_immutable
+    categorical_violations = np.sum(categorical_violations.values, axis=1).reshape(
+        (-1, 1)
+    )  # sum over features
+
+    return (continuous_violations + categorical_violations)
 
 
 def success_rate(counterfactuals):
